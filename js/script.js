67639e9df3a30bbe8fdde916fc015d3853001d7b
nancyabg88 <nancyabg88@gmail.com>
2021-04-15 18:05:23 -0500

    contactenos y js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function validardatos() {
+    /* Comentario */
+
+    //alert("Hola mundo")
+    //prompt("Ingrese su nombre")
+    //console.log("hola mundo")
+    //document.write("HOLA MUNDO")
+
+    let nombre = document.getElementById('name').value
+
+    if(nombre=="Nancy"){
+        alert('Bienvenido')
+    }else{
+        alert('No eres bienvenido');
+    }
+
+
+    
+
+}
